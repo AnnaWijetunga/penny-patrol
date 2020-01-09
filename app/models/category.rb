14f41d47b7d30ee53168502e3278cb9a8501bf01
AnnaWijetunga <annawijetunga@gmail.com>
2020-01-09 12:56:54 -0500

    Includes instance methods for amount and date sort

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,14 @@
 class Category < ActiveRecord::Base
-  
-
+   
   has_many :expenses
   has_many :users, through: :expenses
+
+  def total_amount
+    self.expenses.collect {|expense| expense.amount}.sum
+  end
+
+  def expenses_sort_by_date
+    self.expenses.sort_by {|expense| expense[:date]}.reverse
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   
-  
   has_secure_password
 
   has_many :categories, through: :expenses
@@ -8,6 +7,14 @@ class User < ActiveRecord::Base
 
   def categories_sort_by_name
     self.categories.all.sort_by {|category| category[:name]}
+  end
+
+  def total_amount
+    self.expenses.collect {|expense| expense.amount}.sum
+  end
+
+  def expenses_sort_by_date
+    self.expenses.sort_by {|expense| expense[:date]}.reverse
   end
 
 end
