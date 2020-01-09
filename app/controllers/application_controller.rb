85ef80e0e4d6c2117352749e01a63e64d1497b75
AnnaWijetunga <annawijetunga@gmail.com>
2020-01-09 09:12:50 -0500

    Allow users recognition of being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,49 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "quite_secure"
   end
 
-  get "/" do
-    erb :index
+  get '/' do
+    if !logged_in?
+      erb :index, :layout => :'not_logged_in_layout'
+    else
+      redirect_to_home_page
+    end
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
+    end
+
+    def redirect_to_home_page
+      redirect to "/expenses"
+    end
+
+    def redirect_to_categories
+      redirect to "/categories"
+    end
+
   end
 
 end
